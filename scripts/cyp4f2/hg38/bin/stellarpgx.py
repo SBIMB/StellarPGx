4b26ef178f6c9b0562bba4616a327298914982df
David Twesigomwe <david@n07.core.wits.ac.za>
2023-08-13 18:41:09 +0200

    update CYP4F2 allele definitions

--- scripts/cyp4f2/hg38/bin/stellarpgx.py
+++ scripts/cyp4f2/hg38/bin/stellarpgx.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 from snv_def_modules import *
+from sv_modules import *
 from bkg_modules import *
 
 
@@ -20,10 +21,16 @@ infile = sys.argv[2]
 infile_full = sys.argv[3]
 infile_full_gt = sys.argv[4]
 infile_spec = sys.argv[5]
+sv_del = sys.argv[6]
+sv_dup = sys.argv[7]
+cov_file = sys.argv[8]
+hap_dbs = sys.argv[9]
+act_score = sys.argv[10]
 
 
-cn = 2
+cn = get_total_CN(cov_file)[0]
 
+print("Initially computed CN = {}".format(cn))
 
 supp_core_vars = get_core_variants(infile, cn)
 
@@ -67,4 +74,143 @@ dip_variants = get_all_vars_gt(infile_full_gt)
 
 print("\nResult:")
 
-print(snv_def_alleles)
+# print(snv_def_alleles)
+
+av_cov = get_total_CN(cov_file)[1]
+
+
+gene_alleles = ""
+
+
+if snv_def_alleles != '*1/*1' and cn != '0':
+    in_list = dup_test_init(sv_dup, av_cov)
+
+    
+if cn == '2':
+
+    if 'or' in snv_def_alleles:        
+        print (snv_def_alleles)
+
+    else:
+        gene_alleles = snv_def_alleles
+        print(gene_alleles)
+
+
+elif cn == '0':
+    del_confirm = del_test(sv_del)
+    if del_confirm == '*(full_gene_del)/*(full_gene_del)':
+        gene_alleles = del_confirm
+        print (gene_alleles)
+        
+    elif del_confirm == '*(full_gene_del)':
+        gene_alleles = del_confirm + "/" + "*other"
+        print(gene_alleles)
+
+    else:
+        gene_alleles = "*(full_gene_del)/*(full_gene_del)"
+        print(gene_alleles)
+
+
+elif cn == '1':
+    del_confirm = del_test(sv_del)
+ 
+    if "or" in snv_def_alleles and del_confirm == 'None':
+        print (snv_def_alleles + "\t" + "Possible CYP4F2 gene deletion present")
+
+    elif "or" not in snv_def_alleles and del_confirm == 'None':
+        snv_def_alleles = snv_def_alleles.split("/")
+        snv_cand_alleles = "".join(snv_cand_alleles)
+        snv_cand_alleles = snv_cand_alleles.split("_")
+
+        if snv_def_alleles[0] == snv_def_alleles[1]:
+            gene_alleles = snv_def_alleles[0] + "/" + "*(full_gene_del)"
+            print(gene_alleles)
+
+        elif snv_def_alleles[0] != snv_def_alleles[1]:
+            samp_allele1 = del_adv_test(hap_dbs, snv_cand_alleles[0], snv_cand_alleles[1], snv_def_alleles[0], snv_def_alleles[1], supp_core_vars)
+  
+            gene_alleles = samp_allele1 + "/" + "*(full_gene_del)"
+            print(gene_alleles)
+
+    else:
+        snv_def_alleles = snv_def_alleles.split("/")
+        snv_cand_alleles = "".join(snv_cand_alleles)
+        snv_cand_alleles = snv_cand_alleles.split("_")
+
+        if snv_def_alleles[0] == snv_def_alleles[1]:
+        
+            if del_confirm == "*(full_gene_del)/*(full_gene_del)":
+                del_confirm = "*(full_gene_del)"
+                gene_alleles = del_confirm + "/" + snv_def_alleles[0]
+                print(gene_alleles)
+
+            else:
+                gene_alleles = "*(full_gene_del)" + "/" + snv_def_alleles[0]
+                print(gene_alleles)
+
+        elif snv_def_alleles[0] != snv_def_alleles[1]:
+            samp_allele1 = del_adv_test(hap_dbs, snv_cand_alleles[0], snv_cand_alleles[1], snv_def_alleles[0], snv_def_alleles[1], supp_core_vars)
+    
+            if del_confirm == "*(full_gene_del)/*(full_gene_del)" or del_confirm == "*(full_gene_del)":
+                del_confirm = "*(full_gene_del)"
+                gene_alleles = del_confirm + "/" + samp_allele1
+                print(gene_alleles)
+
+
+
+elif (int(cn) == 3 or int(cn) == 4) and snv_def_alleles != None:
+
+    orig = snv_def_alleles
+
+    if "or" in snv_def_alleles:
+        print (snv_def_alleles + "\n" + "(Duplication present)")
+
+    else:
+        snv_def_alleles = snv_def_alleles.split("/")
+        snv_cand_alleles = "".join(snv_cand_alleles)
+        snv_cand_alleles = snv_cand_alleles.split("_")
+
+        if snv_def_alleles[0] != snv_def_alleles[1]:
+
+            phased_dup = dup_test_cn_3_4(sv_dup, hap_dbs, snv_cand_alleles[0], snv_cand_alleles[1], snv_def_alleles[0], snv_def_alleles[1], cn, av_cov, in_list)
+
+            phased_dup1 = phased_dup.split("/")
+
+
+        elif snv_def_alleles[0] == snv_def_alleles[1]:
+            
+            rt_2 = int(cn) - 1
+            
+            phased_dup = (snv_def_alleles[0] + "/" + snv_def_alleles[1] + "x" + str(rt_2))
+
+
+        gene_alleles = phased_dup
+        
+        print(gene_alleles)
+
+
+elif int(cn) > 4 and snv_def_alleles != None:
+
+    if "or" in snv_def_alleles:
+        print (snv_def_alleles + "\t" + "Duplication present")
+
+    else:
+        snv_def_alleles = snv_def_alleles.split("/")
+        snv_cand_alleles = "".join(snv_cand_alleles)
+        snv_cand_alleles = snv_cand_alleles.split("_")
+
+        if snv_def_alleles[0] != snv_def_alleles[1]:
+
+            phased_dup = dup_test_cn_n(sv_dup, hap_dbs, snv_cand_alleles[0], snv_cand_alleles[1], snv_def_alleles[0], snv_def_alleles[1], cn, av_cov, in_list)
+        elif snv_def_alleles[0] == snv_def_alleles[1]:
+            rt_2 = int(cn) - 1
+            phased_dup = (snv_def_alleles[0] + "/" + snv_def_alleles[1] + "x" + str(rt_2))
+
+        gene_alleles = phased_dup
+        print(phased_dup)
+
+
+
+elif int(cn) > 2 and snv_def_alleles == None:
+    
+    print("Possible rare CYP4F2 structural variant present")
